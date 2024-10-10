ff15396bdf8dfd972ace5675bdaa59b2fd025c52
Warith HARCHAOUI <warith-harchaoui@users.noreply.github.com>
2024-10-10 11:54:55 +0200

    Update setup.py

--- setup.py
+++ setup.py
@@ -6,9 +6,9 @@ with open("README.md", "r") as fh:
 setup(
     name="os-helper",
     version="0.1.0",
-    author="Warith Harchaoui",
-    author_email="your-email@example.com",  # replace with the correct email
-    description="A Python library providing utility functions for working with different operating systems.",
+    author="Warith Harchaoui, Mohamed Chelali and Bachir Zerroug",
+    author_email="warith.harchaoui@gmail.com", 
+    description="OS Helper is a Python library that provides utility functions for working with different operating systems.",
     long_description=long_description,
     long_description_content_type="text/markdown",
     url="https://github.com/warith-harchaoui/os-helper",
