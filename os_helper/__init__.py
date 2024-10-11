b586e2afe63f2a2f887eb7b6cc09039e8a1e46b1
Warith HARCHAOUI <warith-harchaoui@users.noreply.github.com>
2024-10-11 11:37:19 +0200

    Update __init__.py

--- os_helper/__init__.py
+++ os_helper/__init__.py
@@ -1,3 +1,64 @@
+"""
+OS Helper
+
+This package provides a collection of utility functions aimed at simplifying various common programming tasks, including:
+- File handling
+- System operations
+- String manipulation
+- Folder management
+- Hashing
+- URL validation
+
+Authors:
+ - Warith Harchaoui, https://harchaoui.org/warith
+ - Mohamed Chelali, https://mchelali.github.io
+ - Bachir Zerroug, https://www.linkedin.com/in/bachirzerroug
+"""
+
+__all__ = [
+    "verbosity",
+    "windows",
+    "linux",
+    "macos",
+    "unix",
+    "get_nb_workers",
+    "emptystring",
+    "now_string",
+    "temporary_filename",
+    "error",
+    "info",
+    "check",
+    "file_exists",
+    "dir_exists",
+    "absolute2relative_path",
+    "relative2absolute_path",
+    "path_without_home",
+    "recursive_glob",
+    "os_path_constructor",
+    "size_file",
+    "checkfile",
+    "getpid",
+    "copyfile",
+    "remove_directory",
+    "remove_files",
+    "make_directory",
+    "system",
+    "openfile",
+    "hash_string",
+    "hashfile",
+    "hashfolder",
+    "format_size",
+    "folder_description",
+    "folder_name_ext",
+    "valid_config_file",
+    "get_config",
+    "is_working_url",
+    "asciistring",
+    "zip_folder",
+    "time2str"
+]
+
+# Import all the necessary functions from the main module
 from .main import (
     verbosity,
     windows,
@@ -7,14 +68,16 @@ from .main import (
     get_nb_workers,
     emptystring,
     now_string,
-    open,
     temporary_filename,
     error,
     info,
     check,
     file_exists,
     dir_exists,
+    absolute2relative_path,
     relative2absolute_path,
+    path_without_home,
+    recursive_glob,
     os_path_constructor,
     size_file,
     checkfile,
@@ -28,14 +91,13 @@ from .main import (
     hash_string,
     hashfile,
     hashfolder,
+    format_size,
     folder_description,
+    folder_name_ext,
     valid_config_file,
     get_config,
     is_working_url,
-    folder_name_ext,
-    absolute2relative_path,
-    path_without_home,
-    recursive_glob,
+    asciistring,
     zip_folder,
     time2str
 )
